78f83c1bc178d79f32ce57297eaccf3d628b21a3
VenkateshBisoyi <70190415+VenkateshBisoyi@users.noreply.github.com>
2020-12-08 10:13:15 +0530

    Add files via upload

---
+++ L10 - Infix to postfix conversion.py
@@ -0,0 +1,34 @@
+#Infix to Postfix conversion
+print('''Venkatesh Bisoyi
+121910313056''')
+
+infix=input("Enter infix expression(using alphabets in it): ")
+s=[]
+res=""
+prec={'+':1, '-':1, '*':2, '/':2, '^':3}
+def notGreater(h):
+  try:
+    g=prec[h]
+    f=prec[s[-1]]
+    if g<=f:
+      return True
+    else:
+      return False
+  except:
+    return False
+for i in infix:
+  if i=="(":
+    s.append(i)
+  elif i.isalpha():
+    res+=i
+  elif i==")":
+    while s[-1]!="(" and len(s)!=0 :
+      res+=s.pop()
+    s.pop()
+  else:
+    while len(s)!=0 and notGreater(i):
+      res+=s.pop()
+    s.append(i)
+while len(s)>0:
+  res+=s.pop()
+print("Postfix expression:",res)
