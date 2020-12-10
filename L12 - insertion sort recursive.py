fd2554427a410d3b41da39256eb2d6acc50c5259
VenkateshBisoyi <70190415+VenkateshBisoyi@users.noreply.github.com>
2020-12-10 19:22:01 +0530

    Add files via upload

---
+++ L12 - insertion sort recursive.py
@@ -0,0 +1,21 @@
+# Insertion sort recursive
+
+print('''Venkatesh Bisoyi
+121910313056''')
+
+def insertionsort(arr,i,n):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j=j-1
+        arr[j + 1] = key
+        if i + 1 < n:
+            insertionsort(arr, i + 1, n)
+l=[]
+n=int(input("Enter size of the list:"))
+for i in range(n):
+    l.append(int(input("Enter Element:")))
+print("List:",l)
+insertionsort(l,0,n)
+print("Sorted List is: ",l)
